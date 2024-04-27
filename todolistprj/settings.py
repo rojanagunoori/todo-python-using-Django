60db14bf00551cad1bcb8c60f932a6831675eca2
nikhilnagunoori <nikhilnagunoori@gmail.com>
2024-04-27 19:14:10 +0530

    second file

--- todolistprj/settings.py
+++ todolistprj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zy@zgpyd3+088dqz0ymkvh7dwa=$mfgfmk_^^m$=q3_8&b(*$&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
